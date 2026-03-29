05fba94bc788eac72e2584ae3ecb85a68752d842
fproject <fproject@gmail.in>
2026-03-29 14:16:35 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// MySQL connection
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'harshithasql',
+    database: 'library_db'
+});
+
+db.connect(err => {
+    if (err) {
+        console.log("DB connection failed:", err);
+    } else {
+        console.log("Connected to MySQL");
+    }
+});
+
+// 📌 Get all books
+app.get('/books', (req, res) => {
+    db.query("SELECT * FROM books", (err, result) => {
+        if (err) return res.send(err);
+        res.json(result);
+    });
+});
+
+// 📌 Add a book
+app.post('/books', (req, res) => {
+    const { title, author } = req.body;
+    db.query(
+        "INSERT INTO books (title, author) VALUES (?, ?)",
+        [title, author],
+        (err, result) => {
+            if (err) return res.send(err);
+            res.send("Book added");
+        }
+    );
+});
+
+// 📌 Issue book
+app.put('/books/:id', (req, res) => {
+    const id = req.params.id;
+    db.query(
+        "UPDATE books SET available = false WHERE id = ?",
+        [id],
+        (err, result) => {
+            if (err) return res.send(err);
+            res.send("Book issued");
+        }
+    );
+});
+
+app.listen(3000, () => console.log("Server running on port 3000"));
+
+
