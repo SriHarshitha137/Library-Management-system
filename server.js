9fc223aa8a070a547be595839199e1d37f065ce8
fproject <fproject@gmail.in>
2026-03-29 14:34:50 +0530

    updated server.js

--- server.js
+++ server.js
@@ -56,6 +56,10 @@ app.put('/books/:id', (req, res) => {
     );
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 
